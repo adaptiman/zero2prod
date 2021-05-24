6ac8c239c01296aaee7e3bdc8388b1feba5f6b7f
David Sweeney <david@thesweeneys.org>
2021-05-23 21:00:50 -0500

    implements health_check()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -9,6 +13,7 @@ async fn greet(req: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
